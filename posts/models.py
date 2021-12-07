5376524888c891c5e99dd5e9f6eab6d81f5ebd48
Dariusz Dudek <73839751+dariusz-dudek@users.noreply.github.com>
2021-12-07 18:12:15 +0100

    end chapter one

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    published = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
