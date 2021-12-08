f4b9c76491d79f6c7ea6b8444e54a34bad394d77
Dariusz Dudek <73839751+dariusz-dudek@users.noreply.github.com>
2021-12-08 18:28:20 +0100

    before adding argument to command

---
+++ posts/utils.py
@@ -0,0 +1,19 @@
+from faker import Faker
+from .models import Post
+from random import randint
+
+
+def create_posts(n=10):
+    faker = Faker('pl_PL')
+    for _ in range(n):
+        c = faker.date_this_century()
+        post = Post(
+            title=faker.text(randint(10, 30)),
+            content=faker.text(randint(100, 400)),
+            published=faker.boolean(),
+            sponsored=faker.boolean(),
+            created=c,
+            modified=c + faker.time_delta(10)
+        )
+        post.save()
+
