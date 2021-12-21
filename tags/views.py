79d7919cde9d794c9c0b1ad0f7b2ea9c304cb14b
Dariusz Dudek <73839751+dariusz-dudek@users.noreply.github.com>
2021-12-21 10:39:47 +0100

    TagAutocomplete

--- tags/views.py
+++ tags/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from dal import autocomplete
+
+from tags.models import Tag
+
+
+class TagAutocomplete(autocomplete.Select2QuerySetView):
+    def get_queryset(self):
+        if not self.request.user.is_authenticated:
+            return Tag.objects.none()
+
+        qs = Tag.objects.all()
+
+        if self.q:
+            qs = qs.filter(name__istartswith=self.q)
+
+        return qs
