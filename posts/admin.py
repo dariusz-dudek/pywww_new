9109bfe09b66ddcfca0878abc4d31467b671ba8a
Dariusz Dudek <73839751+dariusz-dudek@users.noreply.github.com>
2021-12-21 09:04:11 +0100

    autocomplete fields on tags

--- posts/admin.py
+++ posts/admin.py
@@ -14,5 +14,5 @@ class PostAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ['id', 'title', 'created', 'modified', 'published', 'sponsored']
     search_fields = ['title', 'description']
     list_filter = ['published', 'sponsored']
-    filter_horizontal = ['tags']
+    autocomplete_fields = ('tags', )
     resource_class = PostResource
