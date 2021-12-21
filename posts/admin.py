f46314bab117496c103043e2dfa471a98a9e81d1
Dariusz Dudek <73839751+dariusz-dudek@users.noreply.github.com>
2021-12-21 09:01:26 +0100

    filter horizontal in tags

--- posts/admin.py
+++ posts/admin.py
@@ -14,4 +14,5 @@ class PostAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ['id', 'title', 'created', 'modified', 'published', 'sponsored']
     search_fields = ['title', 'description']
     list_filter = ['published', 'sponsored']
+    filter_horizontal = ['tags']
     resource_class = PostResource
